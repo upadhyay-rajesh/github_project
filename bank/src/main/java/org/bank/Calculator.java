b51db88b4c520575ca98d50b07c13ef876bfa228
rajesh <upadhyay.rajesh@rediffmail.com>
2018-12-20 10:03:25 +0530

    sa

---
+++ bank/src/main/java/org/bank/Calculator.java
@@ -0,0 +1,14 @@
+package org.bank;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		Calculator c=new Calculator();
+		c.add();
+
+	}
+	public void add(){
+		System.out.println("add");
+	}
+
+}
